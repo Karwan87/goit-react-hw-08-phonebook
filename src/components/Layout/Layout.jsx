f911cde595f964a4cfee2c7f62dfbfad51d7d6d6
Karwan87 <damianuszynski@gmail.com>
2023-08-01 23:09:56 +0200

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import Navigation from 'components/Navigation/Navigation';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navigation />
+      <div>
+        <Suspense fallback={null}>
+          {}
+          <Outlet />
+        </Suspense>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
