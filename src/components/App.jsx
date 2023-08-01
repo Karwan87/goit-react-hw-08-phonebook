87a4d8351a956a8bf9c25e020577834643e775f4
Karwan87 <damianuszynski@gmail.com>
2023-08-01 23:09:10 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
+import React from 'react';
+import { lazy, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
+import { PrivateRoute } from 'PrivateRoute';
+import { RestrictedRoute } from 'RestrictedRoute';
+import { refreshUser } from '../redux/auth/operations';
+import Home from '../pages/HomePage/Home';
+import Layout from './Layout/Layout';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
+document.title = 'PhonebookBox_redux';
+
+const RegisterPage = lazy(() => import('../pages/RegisterPage/Register'));
+const LoginPage = lazy(() => import('../pages/LoginPage/Login'));
+const ContactsPage = lazy(() => import('../pages/ContactsPage/Contacts'));
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegisterPage />}
+              />
+            }
+          />
+
+          <Route
+            path="login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+
+          <Route
+            path="contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+export default App;
